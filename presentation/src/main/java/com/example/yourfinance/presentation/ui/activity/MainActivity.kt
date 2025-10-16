08158d2b4a886aaa19bd1a305eca481dd719ebd5
Zapruto Evgenii <zapruto.evgenii@gmail.com>
2025-10-16 16:50:08 +0800

    ci/cd

--- presentation/src/main/java/com/example/yourfinance/presentation/ui/activity/MainActivity.kt
+++ presentation/src/main/java/com/example/yourfinance/presentation/ui/activity/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.yourfinance.presentation.ui.activity
 
-//import12323333ssц231
+//import12323333ssц2314
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
